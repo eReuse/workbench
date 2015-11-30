41d8340e33f1ea63fd9896e49c0d83e0019ae1aa
santiago <santiago@pangea.org>
2015-11-30 16:51:13 +0100

    Retrieve SMART short offline result.

--- device_inventory/benchmark.py
+++ device_inventory/benchmark.py
@@ -6,6 +6,7 @@ performance of an object, normally by running a number of standard
 tests and trials against it.
 
 """
+import logging
 import re
 import subprocess
 
@@ -16,32 +17,54 @@ def hard_disk_smart(disk="/dev/sda"):
     # smartctl -a /dev/sda | grep "# 1"
     # # 1  Short offline       Completed without error       00%     10016         -
     # XXX extract data of smartest. Decide which info is relevant.
+    error = False
     try:
         smart = subprocess.check_output(["smartctl", "-a", disk],
                                         universal_newlines=True)
     except subprocess.CalledProcessError as e:
-        # TODO analyze e.returncode
         smart = e.output
+        # analyze e.returncode
+        if e.returncode == pow(2, 0):  # bit 0
+            raise  # command line did not parse
+        elif e.returncode == pow(2, 1):  # bit 1
+            pass  # only warning because low-power
+        elif e.returncode == pow(2, 2):  # bit 2
+            error = True  # TODO cannot perform SMART
+        else: # bit 3, 4, 5, 6, 7  device log with errors
+            error = True
     
-    # current output
-    # Num  Test_Description  Status  Remaining  LifeTime(hours)  LBA_of_first_error
-    beg = smart.index('# 1')
-    end = smart.index('\n', beg)
-    result = re.split(r'\s\s+', smart[beg:end])
-    
-    try:
-        lba_first_error = int(result[5], 0)  # accepts hex and decimal value
-    except ValueError:
-        lba_first_error = None
-    
-    return {
+    test = {
         "@type": "TestHardDrive",
-        "device": disk,
-        "type": result[1],
-        "status": result[2],
-        "lifetime": result[4],
-        "firstError": lba_first_error,
+        #"device": disk,
+        "error": error,
     }
+
+    # excepted output
+    # Num  Test_Description  Status  Remaining  LifeTime(hours)  LBA_of_first_error
+    try:
+        beg = smart.index('# 1')
+        end = smart.index('\n', beg)
+        result = re.split(r'\s\s+', smart[beg:end])
+    except ValueError:
+        logging.error("Error retrieving SMART info from '%s'", disk)
+    else:
+        try:
+            lifetime = int(result[4])
+        except ValueError:
+            lifetime = -1
+        try:
+            lba_first_error = int(result[5], 0)  # accepts hex and decimal value
+        except ValueError:
+            lba_first_error = None
+        
+        test.update({
+            "type": result[1],
+            "status": result[2],
+            "lifetime": lifetime,
+            "firstError": lba_first_error,
+        })
+    
+    return test
 
 
 def score_cpu():
