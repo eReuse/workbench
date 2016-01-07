713cdcd743dcc8c3740183bb4fef5e6d97d25240
santiago <santiago@pangea.org>
2016-01-07 10:04:49 +0100

    Fix filesystem.manifest generation

--- scripts/build_ereuse_iso.sh
+++ scripts/build_ereuse_iso.sh
@@ -97,7 +97,7 @@ sudo umount edit/dev
 
 # Regenerate manifest
 sudo chmod +w extract-cd/casper/filesystem.manifest
-sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
+sudo chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee extract-cd/casper/filesystem.manifest
 sudo cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
 sudo sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
 sudo sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
