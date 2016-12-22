64fa3b3894e6f0358d2f8ddf16a013adc1e2f8c5
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2016-12-22 12:13:54 +0100

    Include current eReuseOS ISO in data archive.
    Along goes a SYSLINUX/PXELINUX template for it.

--- scripts/build_ereuse_data.sh
+++ scripts/build_ereuse_data.sh
@@ -11,6 +11,17 @@ BUILD_DIR=$(mktemp -d)
 mkdir -p $BUILD_DIR/ereuse-data/inventory $BUILD_DIR/ereuse-data/images
 cp device_inventory/config.ini $BUILD_DIR/ereuse-data
 
+IMAGE=eReuseOS
+cp dist/iso/$IMAGE-$VERSION.iso $BUILD_DIR/ereuse-data/images/$IMAGE.iso
+cat > $BUILD_DIR/ereuse-data/images/$IMAGE.iso.syslinux << EOF
+LABEL $IMAGE
+    MENU LABEL $IMAGE
+        kernel mnt/$IMAGE/casper/vmlinuz
+        initrd mnt/$IMAGE/casper/initrd.lz
+        append ip=dhcp netboot=nfs nfsroot=@ADDRESS@:@TFTPBOOT@/mnt/$IMAGE boot=casper text forcepae
+        IPAPPEND 2
+EOF
+
 chmod -R a+rX $BUILD_DIR
 
 tar -zcf dist/ereuse-data-$VERSION.tar.gz -C $BUILD_DIR ereuse-data
