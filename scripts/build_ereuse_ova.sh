1afd8f24e4487ecb14b65b1a7b8214fc84e4d613
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2017-01-02 11:19:52 +0100

    New incomplete script to build PXE server OVA (#60).

---
+++ scripts/build_ereuse_ova.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+# Generate OVA of eReuse PXE server.
+
+set -e
+
+# Configurable settings.
+WORK_DIR=${WORK_DIR:-dist/iso}
+DISK_MiB=${DISK_MiB:-2048}  # VM disk size in MiB
+SWAP_MiB=${SWAP_MiB:-128}  # VM swap size in MiB
+
+BASE_ISO_URL="http://ubuntu-mini-remix.mirror.garr.it/mirrors/ubuntu-mini-remix/15.10/ubuntu-mini-remix-15.10-i386.iso"
+BASE_ISO_SHA256="e9985f0bcb05678d87d62c3d70191aab7a80540dc17523d93c313aa8515e173e"
+
+# Other derived values.
+BASE_ISO_PATH="$WORK_DIR/$(basename "$BASE_ISO_URL")"
+BASE_ISO_SHA256SUM="$BASE_ISO_SHA256  $BASE_ISO_PATH"
+
+
+# Download the base ISO.
+while ! echo "$BASE_ISO_SHA256SUM" | sha256sum -c --quiet --status; do
+    wget -c -O $BASE_ISO_PATH "$BASE_ISO_URL"
+done
+
+# Create a temporary directory for data files.
+DATA_DIR=$(mktemp -d -p"$WORK_DIR")
+
+# Create and partition the VM disk image.
+DISK_IMAGE="$DATA_DIR/disk.raw"  # VM disk image
+dd if=/dev/zero "of=$DISK_IMAGE" bs=1M count=0 "seek=$DISK_MiB"  # sparse
+swap_first_sector=$(((DISK_MiB - SWAP_MiB) * 1024 * 2))
+default=  # just for readability
+cat << EOF | tr ' ' '\n' | fdisk "$DISK_IMAGE" > /dev/null
+n p 2 $swap_first_sector $default
+n p 1 $default $default
+t 2 82
+w
+EOF
+
+# Create a file system and a swap space in the VM disk.
+disk_loop=$(losetup -fP --show "$DISK_IMAGE")
+mkfs.ext4 -q ${disk_loop}p1
+mkswap ${disk_loop}p2
+
+
+ROOT="$DATA_DIR/root"  # mounted VM root file system
+mkdir -p "$ROOT"
+mount ${disk_loop}p1 "$ROOT"
+
+# chroot "$ROOT" /bin/sh  # still empty
+
+# Unmount the file system and release the loop device.
+umount "$ROOT"
+losetup -d $disk_loop
