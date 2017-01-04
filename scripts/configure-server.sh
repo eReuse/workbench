6895d6b95197c84ae8b210247dd63bd9d25624af
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2017-01-04 13:09:59 +0100

    Install VirtualBox guest additions, cleanup.
    This is needed for the data shared folder.

--- scripts/configure-server.sh
+++ scripts/configure-server.sh
@@ -6,6 +6,18 @@
 # Remove unnecessary packages.
 apt-get -qq purge @PKGS_TO_REMOVE@
 
+# Install needed packages.
+# Enable additional packages like VirtualBox's.
+sed -i -e 's/ main/ main multiverse/' /etc/apt/sources.list
+apt-get update
+apt-get -qq --no-install-recommends install virtualbox-guest-dkms
+
+# Enable VirtualBox's shared folder module.
+cat << 'EOF' > /etc/modules-load.d/ereuse
+# To share eReuse's data folder with the VirtualBox host.
+vboxsf
+EOF
+
 # Rebuild initramfs if missing.
 if [ ! -f /initrd.img ]; then
     update-initramfs -u
@@ -19,6 +31,9 @@ grub-install $(findmnt -no SOURCE / | sed -r 's/p?[0-9]+$//')
 printf 'eReuse\neReuse\n' | passwd -q root
 adduser --disabled-password --gecos eReuse ereuse
 printf 'ereuse\nereuse\n' | passwd -q ereuse
+
+# Cleanup.
+apt-get clean  # downloaded package files
 
 # Restore the original init script.
 mv /etc/rc.local.orig /etc/rc.local
