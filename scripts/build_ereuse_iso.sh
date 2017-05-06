71f1a0f1def25615cc900e5c53ca9f8797892376
Garito <garito@sistes.net>
2017-05-07 01:22:14 +0200

    Don't run as script. Let's try if we can run the python way

--- scripts/build_ereuse_iso.sh
+++ scripts/build_ereuse_iso.sh
@@ -104,9 +104,9 @@ ch cat > $FS_ROOT/etc/systemd/WorkbenchSneaky.conf << "EOF"
 start on runlevel [2345]
 stop on runlevel [!2345]
 
-exec /home/ubuntu/WorkbenchSneaky/sneaky.py http://192.168.2.2:5000
+exec python /home/ubuntu/WorkbenchSneaky/sneaky.py http://192.168.2.2:5000
 EOF
-ch chmod a+x /home/ubuntu/WorkbenchSneaky/sneaky.py
+#ch chmod a+x /home/ubuntu/WorkbenchSneaky/sneaky.py
 
 # ereuse-workbench requirements
 # TODO read from requirements.txt
--- scripts/configure-server.sh
+++ scripts/configure-server.sh
@@ -154,9 +154,8 @@ cat > /etc/systemd/WorkbenchFS.conf << EOF
 start on runlevel [2345]
 stop on runlevel [!2345]
 
-exec $data_user_home/WorkbenchFS/app.py
+exec python $data_user_home/WorkbenchFS/app.py
 EOF
-chmod a+x $data_user_home/WorkbenchFS/app.py
 
 # Cleanup and restore the original init script.
 if [ $vm = yes ]; then
