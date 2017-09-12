ec0dcea18234ae69cce130756a1c3132f3f918e0
Garito <garito@gmail.com>
2017-09-12 18:14:14 +0200

    Update Ubuntu version to 17.04 cuase the 15.10 version has been disapered

--- scripts/build_ereuse_iso.sh
+++ scripts/build_ereuse_iso.sh
@@ -16,8 +16,13 @@ WORK_DIR=${WORK_DIR:-dist/iso}
 
 # Version-specific settings.
 VERSION=$(cd erwb && python -Bc 'from __init__ import get_version; print get_version()')
-BASE_ISO_URL="http://ubuntu-mini-remix.mirror.garr.it/mirrors/ubuntu-mini-remix/15.10/ubuntu-mini-remix-15.10-i386.iso"
-BASE_ISO_SHA256="e9985f0bcb05678d87d62c3d70191aab7a80540dc17523d93c313aa8515e173e"
+
+# Seems that this iso has been dissapeared
+# BASE_ISO_URL="http://ubuntu-mini-remix.mirror.garr.it/mirrors/ubuntu-mini-remix/15.10/ubuntu-mini-remix-15.10-i386.iso"
+# BASE_ISO_SHA256="e9985f0bcb05678d87d62c3d70191aab7a80540dc17523d93c313aa8515e173e"
+# Update to Zesty Zapus
+BASE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/zesty/main/installer-i386/current/images/netboot/mini.iso"
+BASE_ISO_SHA256="41a9579525680f496f4ac752a3b58e9964a3f010"
 
 # Other derived values.
 # This line is obsolete in favor of git clone
