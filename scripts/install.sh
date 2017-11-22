2f1b1bbed69b815e02cd086e851214e5eafb6b7b
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-11-22 19:26:09 +0100

    Remove git submodule init/update

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,20 +11,17 @@ cd ${1:-'./..'}
 echo Installing debian required packages...
 cat debian-requirements.txt | xargs sudo apt install -y --no-install-recommends
 
-
 echo Installing python packages...
 pip install -e . -r requirements.txt
-
 
 echo Installing erwb command line...
 install -m 0755 scripts/erwb /usr/local/sbin/erwb
 # Example: sudo erwb --settings /media/ereuse-data/config.ini --inventory /media/ereuse-data/inventory
 
 echo Installing reciclanet scripts...
-git submodule init
-git submodule update
-
-# Reciclanet's scripts
+echo 'Ensure you have performed git submodule init / git submodule update'
+#git submodule init
+#git submodule update
 install -m 0755 reciclanet-scripts/instalar /usr/local/bin/erwb-install-image
 
 echo Workbench installed!
